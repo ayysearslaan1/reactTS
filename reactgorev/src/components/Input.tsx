cf19ac3ee175ee6d72212fb87132b70e6f5f3c24
ayysearslaan1 <114736890+ayysearslaan1@users.noreply.github.com>
2023-03-16 11:45:42 +0300

    typescript eğitim

---
+++ reactgorev/src/components/Input.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Input=() => {
+    return(
+        <div>
+            <input type="text" placeholder='Ad' />
+            <input type="text" placeholder='soyad' />
+            <input type="text" placeholder='numara' />
+            <button>ekle</button>
+        </div>
+    )
+}
+  
+   
+export default Input
+
